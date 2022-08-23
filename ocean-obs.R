a8ede63033b60742f8deff4ae6f22dbb81244373
d-siden <danilo.siden@gmail.com>
2022-08-23 12:13:29 -0300

    e agora?

--- ocean-obs.R
+++ ocean-obs.R
@@ -1,8 +1,34 @@
 # obter (e exibir, opcional) dados em código SHIP e boias
 # https://www.ndbc.noaa.gov/ship_obs.php?uom=M&time=12
-# https://www.ndbc.noaa.gov/
 # https://www.dataquest.io/blog/web-scraping-in-r-rvest/
-# Acessar o site
-con <- url('https://www.ndbc.noaa.gov/ship_obs.php?uom=M&time=0')
 
-linhas <- readLines(con)
+# Trouxe a ferramenta?
+for (p in c("stringr")){
+  if (!(p %in% installed.packages())){
+    install.packages(p)
+  }
+  library(p, character.only = T)
+}
+
+# Adentrar o recinto
+recinto <- url('https://www.ndbc.noaa.gov/ship_obs.php?uom=M&time=0')
+
+# Observar o ambiente
+recinto <- readLines(recinto)
+recinto <- data.frame(recinto)
+
+# Separar os elementos
+for (elemento in recinto$recinto) {
+  # Procurar o título
+  if (grepl("SHIP", elemento, fixed = T) ){
+    print(elemento)
+    cat("
+    ")
+  }
+  # pegar centesimo elemento por razões
+  # dividir pela \
+  strsplit(recinto$recinto[100], split = "\"")
+  # que fazer com os subelementos?
+  
+  
+}
