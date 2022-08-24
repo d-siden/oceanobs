1179ac30869e0f5ad79d9fb6d0a0a71e34a10713
d-siden <danilo.siden@gmail.com>
2022-08-24 11:42:34 -0300

    eba

--- ocean-obs.R
+++ ocean-obs.R
@@ -2,7 +2,36 @@
 
 # lembrar de setar o pwd no terminal para documents
 
-# Adentrar o recinto
-recinto <- read.table(url('https://www.ndbc.noaa.gov/data/realtime2/ship_obs.txt'))
+cabecalho <- c("SHIP_ID", "YY", "MM", "DD", "hh",
+               "LAT", "LON", "WDIR", "WSPD", "GST", "WVHT",
+               "DPD", "APD", "MWD", "PRES", "ATMP", "WTMP",
+               "DEWP", "VIS", "PTDY", "TCC", "S1HT",
+               "S1PD", "S1DIR", "S2HT", "S2PD", "S2DIR", "II",
+               "IE", "IR", "IC", "IS", "Ib", "ID", "Iz"
+)
+unidades <- c("(estacao)", "(ano)", "(mes)", "(dia)", "(hora)",
+               "(°)", "(°)", "(°T)", "(m/s)", "(m/s)", "(m)",
+               "(s)", "(s)", "(°T)", "(hPa)", "(°C)", "(°C)",
+               "(°C)", "(NM)", "(hPa)", "(octa)", "(m)",
+               "(s)", "(°T)", "(m)", "(s)", "(°T)", "II",
+               "IE", "IR", "IC", "IS", "Ib", "ID", "Iz"
+               )
+names(unidades) <- cabecalho
 
+
+obs <- read.table(
+  url('https://www.ndbc.noaa.gov/data/realtime2/ship_obs.txt'),
+  header = F,
+  dec = ".",
+  na.strings = "MM",
+  stringsAsFactors = F
+  )
+
+if (ncol(obs) != length(cabecalho)){
+  stop("Cabeçalho não confere com os dados!")
+}else{
+  # colocar o cabeçalho no dataframe
+  names(obs) <- cabecalho
+  
+}
 
