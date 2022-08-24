0888669a2f186fcbd9dbc7c6c0dc3fd5999c72a9
d-siden <danilo.siden@gmail.com>
2022-08-24 10:53:12 -0300

    agora sim

--- ocean-obs.R
+++ ocean-obs.R
@@ -1,36 +1,8 @@
 # obter (e exibir, opcional) dados em código SHIP e boias
 
-# https://www.ndbc.noaa.gov/ship_obs.php?uom=M&time=12
-
 # lembrar de setar o pwd no terminal para documents
 
-# Trouxe a ferramenta?
-for (p in c("stringr")){
-  if (!(p %in% installed.packages())){
-    install.packages(p)
-  }
-  library(p, character.only = T)
-}
-
 # Adentrar o recinto
-recinto <- url('https://www.ndbc.noaa.gov/ship_obs.php?uom=M&time=0')
+recinto <- read.table(url('https://www.ndbc.noaa.gov/data/realtime2/ship_obs.txt'))
 
-# Observar o ambiente
-recinto <- readLines(recinto)
-recinto <- data.frame(recinto)
 
-# Separar os elementos
-for (elemento in recinto$recinto) {
-  # Procurar o título
-  if (grepl("SHIP", elemento, fixed = T) ){
-    print(elemento)
-    cat("
-    ")
-  }
-  # pegar centesimo elemento por razões
-  # dividir pela \
-  strsplit(recinto$recinto[100], split = "\"")
-  # que fazer com os subelementos?
-  
-  
-}
